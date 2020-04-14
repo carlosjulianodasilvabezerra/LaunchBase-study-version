f1089dc2441ef7d2693fb157562d234334af49e5
carlosjulianodasilvabezerra <carlos_julianoti@outlook.com>
2020-04-14 07:51:00 -0300

    first commit

---
+++ back-end/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    id: "Fbu7z5dXcRs",
+    title: "SQL no Node.js com Sequelize",
+    duration: "1:140:21 hrs",
+    price: "Free"
+  },
+  {
+    id: "3tB0uDliS6Y",
+    title: "Masterizando O Insomnia",
+    duration: "34:24 min",
+    price: "Free"
+  },
+  {
+    id: "ixbvCw-YiOA",
+    title: "AdonisJS, vale a pena utilizar?",
+    duration: "1:140:21 hrs",
+    price: "Free"
+  },
+  {
+    id: "-c57D2kQffQ",
+    title: "Automatizando padrões de código com Husky + lint-staged",
+    duration: "22:03 min",
+    price: "Free"
+  },
+  {
+    id: "eSjFDWYkdxM",
+    title: "Emulando React Native no iOS/Android com Expo | Code/Drops",
+    duration: "16:04 min",
+    price: "Free"
+  },
+  {
+    id: "bg-U0xZwcRk",
+    title: "Clonando interface do Uber com React Native",
+    duration: "1:23 hrs",
+    price: "Free"
+  }
+]
---
+++ back-end/server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+const videos = require('./data')
+
+server.use(express.static('public'))
+
+server.set('view engine', 'njk')
+
+nunjucks.configure('views', {
+  autoescape: false,
+  express: server
+})  
+
+server.get('/', (req, res) => {
+  const about = {
+    avatar_url: "https://cdn.icon-icons.com/icons2/108/PNG/256/males_male_avatar_man_people_faces_18313.png",
+    name: "Carlos J S Bezerra",
+    role: "Instrutor - Rocketseat",
+    description: 'Programmer full-stack, focado em trazer o melhor ensino para iniciantes <a href="https://rocketseat.com.br" target="_blank">rocketseat</a>',
+    links: [
+      { name: 'Github', url: 'https://github.com/carlosjulianodasilvabezerra/'},
+      { name: 'twitter', url: 'https://github.com/carlosjulianodasilvabezerra/'},
+      { name: 'linkedin', url: 'https://www.linkedin.com/in/carlos-juliano-bezerra-b9294b13b/'}
+    ]
+  }
+
+  return res.render('about', { about })
+})
+
+server.get('/portfolio', (req, res) => {
+  return res.render('portfolio', { items: videos })
+})
+
+server.listen(3000, function () {
+  console.log('Server is running')
+})
